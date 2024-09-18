c0ee606eaa1b646c0e6549c5c08158096ddf0161
Lise <202207164@post.au.dk>
2024-09-18 12:37:59 +0200

    added all files

---
+++ .ipynb_checkpoints/startup-checkpoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# any python packages you want to have preinstalled
+#pip install pingouin
+#pip install lxml
+
+# your git config information
+git config --global user.email "202207164@post.au.dk"
+git config --global user.name "Lise"
